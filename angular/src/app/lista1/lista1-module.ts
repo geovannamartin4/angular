499f3a431c45a1a0a4f5d95f02cf3c27b80b1172
Geovanna <125368602+geovannamartin4@users.noreply.github.com>
2026-09-02 01:37:02 +0000

    Resolvendo o exercicio 4

--- angular/src/app/lista1/lista1-module.ts
+++ angular/src/app/lista1/lista1-module.ts
@@ -5,9 +5,10 @@ import { Lista1RoutingModule } from './lista1-routing-module';
 import { Ex1 } from './ex1/ex1';
 import { Ex2 } from './ex2/ex2';
 import { Ex3 } from './ex3/ex3';
+import { Ex4 } from './ex4/ex4';
 
 @NgModule({
-  declarations: [Ex1, Ex2, Ex3],
+  declarations: [Ex1, Ex2, Ex3, Ex4],
   imports: [CommonModule, Lista1RoutingModule],
 })
 export class Lista1Module {}
--- angular/src/app/lista1/lista1-routing-module.ts
+++ angular/src/app/lista1/lista1-routing-module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { Ex1 } from './ex1/ex1';
 import { Ex2 } from './ex2/ex2';
 import { Ex3 } from './ex3/ex3';
+import { Ex4 } from './ex4/ex4';
 
 
   const routes: Routes = [
   
     { path: 'ex1', component: Ex1 },
     {path: 'ex2', component: Ex2},
-    {path: 'ex3', component: Ex3}
+    {path: 'ex3', component: Ex3},
+    {path: 'ex4', component: Ex4}
   
   ];
 
