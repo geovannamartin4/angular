b698d1a54788af3aa299e580fc5bd366dc2d93ab
Geovanna <125368602+geovannamartin4@users.noreply.github.com>
2026-09-02 01:25:19 +0000

    Resolvendo exercicio 3

---
+++ angular/src/app/lista1/ex3/ex3.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex3',
+  standalone: false,
+  templateUrl: './ex3.html',
+  styleUrl: './ex3.css',
+})
+export class Ex3 {
+  imagemProduto = 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcT7OTgoAmsarsITmtrRCXLKMXHZqrNi0TCNgk8xpiGs0li3VyYXNvD6-3XzKS8b5ZSqnScfYUoCbcCUawEAM19YQPifcGgIp29BDdrm_coySzYita51n12si7w';
+  descricaoProduto = 'Teclado Razer';
+}
--- angular/src/app/lista1/lista1-module.ts
+++ angular/src/app/lista1/lista1-module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { Lista1RoutingModule } from './lista1-routing-module';
 import { Ex1 } from './ex1/ex1';
 import { Ex2 } from './ex2/ex2';
+import { Ex3 } from './ex3/ex3';
 
 @NgModule({
-  declarations: [Ex1, Ex2],
+  declarations: [Ex1, Ex2, Ex3],
   imports: [CommonModule, Lista1RoutingModule],
 })
 export class Lista1Module {}
