3a2cf9a41c766b9e9984cdd8446f158e41f1bd7d
Geovanna <125368602+geovannamartin4@users.noreply.github.com>
2026-09-04 19:24:14 +0000

    Resolvendo exercicio 10

---
+++ angular/src/app/lista1/ex10/ex10.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex10',
+  standalone: false,
+  templateUrl: './ex10.html',
+  styleUrl: './ex10.css',
+})
+export class Ex10 {
+
+  usuario = '';
+  senha = '';
+  mensagem = '';
+
+  entrar() {
+    this.mensagem = 'Usuário: ' + this.usuario;
+  }
+
+}
