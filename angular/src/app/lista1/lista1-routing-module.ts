3160dab6b6bdae2921ee45662cfc92c908e3255c
Geovanna <125368602+geovannamartin4@users.noreply.github.com>
2026-09-04 17:50:07 +0000

    Resolvendo exercicio 7

--- angular/src/app/lista1/lista1-routing-module.ts
+++ angular/src/app/lista1/lista1-routing-module.ts
@@ -6,6 +6,8 @@ import { Ex3 } from './ex3/ex3';
 import { Ex4 } from './ex4/ex4';
 import { Ex5 } from './ex5/ex5';
 import { Ex6 } from './ex6/ex6';
+import { Ex7 } from './ex7/ex7';
+
 
 
   const routes: Routes = [
@@ -15,7 +17,8 @@ import { Ex6 } from './ex6/ex6';
     {path: 'ex3', component: Ex3},
     {path: 'ex4', component: Ex4},
     {path: 'ex5', component: Ex5},
-    {path: 'ex6', component: Ex6}
+    {path: 'ex6', component: Ex6},
+    {path: 'ex7', component: Ex7}
   
   ];
 
